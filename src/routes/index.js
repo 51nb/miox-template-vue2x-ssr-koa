67b08710c0d222781aa58928e8cf53340a13efba
shenyunjie <shenyunjie@u51.com>
2017-11-01 15:30:37 +0800

    update

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,6 @@
 import Router from 'miox-router';
-
 import HomePage from '../webviews/home/index.vue';
 
 const route = new Router();
-
 route.patch('/', async ctx => await ctx.render(HomePage));
-
 export default route;
